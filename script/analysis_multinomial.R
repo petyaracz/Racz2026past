d26f3beba95f3cfca6ed7f5302ca767343f9f9a2
Péter Rácz <petermartonracz@gmail.com>
2025-11-14 11:26:38 +0100

    nice

--- script/analysis_multinomial.R
+++ script/analysis_multinomial.R
@@ -9,9 +9,9 @@ library(ggeffects)
 library(broom.mixed)
 library(ggthemes)
 library(patchwork)
+library(ggeffects)
 
 # -- plotting fun -- #
-
 
 plotCont = function(fit,pred_name){
   ggpredict(fit, terms = pred_name) |> 
@@ -63,22 +63,59 @@ d2 = d |>
       fct_reorder(year) |> ordered()
   )
 
-# -- fit -- #
+# -- corr -- #
 
-fit3 = brm(
-  verb_past_class_complex ~ log_lemma_freq_scaled + translation + number + 
-    person + prefix + motion_verb + communication_verb + modal_verb + 
-    lemma_length,
-  data = d2,
-  family = categorical(link = "logit", refcat = "ipf"),
-  chains = 4,
-  iter = 2000
-)
+# -- corr -- #
 
-# -- save -- #
+d2 |> 
+  mutate(
+    prefix = prefix != 'other verb',
+    modal_verb = modal_verb != 'other verb',
+    motion_verb = motion_verb != 'other verb',
+    communication_verb = communication_verb != 'other verb',
+    number = number == 'S',
+    person = as.double(person),
+    translation_MunchK = translation == 'MunchK',
+    translation_JordK = translation == 'JordK',
+    translation_Pesti = translation == 'Pesti',
+    translation_Sylvester = translation == 'Sylvester',
+    translation_Heltai = translation == 'Heltai',
+    translation_Karoli = translation == 'Karoli',
+    translation_Kaldi = translation == 'Kaldi'
+  ) |> 
+  select(lemma_length,log_lemma_freq_scaled,prefix,modal_verb,motion_verb,communication_verb,number,person,all_of(starts_with('translation_'))) |> 
+  cor(method = 'spearman') |> 
+  as.data.frame() |> 
+  rownames_to_column() |> 
+  pivot_longer(-rowname) |> 
+  ggplot(aes(rowname,name,fill = value, label = round(value,2))) +
+  geom_tile() +
+  geom_text(colour = 'darkgrey') +
+  scale_fill_viridis_b(n.breaks = 6) +
+  theme_few() +
+  labs(fill = 'Spearman r') +
+  xlab('') +
+  ylab('') +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
-saveRDS(fit3, 'models/fit3.rds')
-summary(fit3)
+ggsave('viz/cors_multi.png', dpi = 'print', width = 10, height = 8)
+
+# # -- fit -- #
+# 
+# fit3 = brm(
+#   verb_past_class_complex ~ log_lemma_freq_scaled + translation + number + 
+#     person + prefix + motion_verb + communication_verb + modal_verb + 
+#     lemma_length,
+#   data = d2,
+#   family = categorical(link = "logit", refcat = "ipf"),
+#   chains = 4,
+#   iter = 2000
+# )
+# 
+# # -- save -- #
+# 
+# saveRDS(fit3, 'models/fit3.rds')
+# summary(fit3)
 
 # -- load -- #
 
@@ -88,6 +125,24 @@ fit3 = readRDS('models/fit3.rds')
 
 tidy(fit3, conf.int = T) |> 
   write_tsv('dat/best_model_coef_multi.tsv')
+
+# -- vif -- #
+
+X = model.matrix(~ log_lemma_freq_scaled + translation + number + person + 
+                    prefix + motion_verb + communication_verb + modal_verb + 
+                    lemma_length, 
+                  data = d2)
+
+# Remove intercept column
+X = X[, -1]
+
+# Fit a dummy model and check VIF
+dummy_model = lm(rnorm(nrow(X)) ~ ., data = as.data.frame(X))
+car::vif(dummy_model)
+
+# "We assessed collinearity by calculating variance inflation factors from the predictor design matrix. Standard VIF diagnostics are not directly applicable to multinomial models, so we computed VIF using the predictor matrix in a linear model framework (car package in R). All VIF values were below 5."
+# escalate:
+# "VIF diagnostics for multinomial models are not well-established in the literature. We verified collinearity using multiple approaches including pairwise correlations and VIF calculated from the design matrix. All diagnostics indicated no collinearity issues."
 
 # -- viz -- #
 
