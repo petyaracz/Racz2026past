408e760d85486386969c4b0470fd0d9174a6ec0a
Péter Rácz <petermartonracz@gmail.com>
2025-12-08 07:46:06 +0100

    nice plots

--- script/viz_counts.R
+++ script/viz_counts.R
@@ -3,47 +3,89 @@
 setwd('~/Github/Racz2026past/')
 
 library(tidyverse)
-library(lme4)
-library(sjPlot)
-library(performance)
+library(ggthemes)
 
 # -- read -- #
 
 d = read_tsv('dat/gospel_and_dict.tsv.gz')
+tmk = read_tsv('dat/tmk_and_dict.tsv.gz')
 
 # -- counts -- #
 
-counts1 = d |> 
-  count(verb_past_class_complex,translation,year,book,chapter) |> 
-  mutate(translation = fct_reorder(translation,year))
+c1 = d |> 
+  count(lemma,lemma_freq,verb_past_class_complex) |> 
+  mutate(source = 'Gospels')
 
-counts2 = counts1 |> 
+c2 = tmk |> 
+  count(lemma,verb_past_class_complex) |> 
+  mutate(source = 'informal language')
+
+counts = bind_rows(c1,c2)
+
+implemma = c1 |> 
+  pivot_wider(names_from = verb_past_class_complex, values_from = n, values_fill = 0) |> 
+  mutate(
+    sum = `complex imperfective` + `complex perfective` + imperfective + perfective,
+    imppercent = imperfective/sum
+  ) |> 
+  select(lemma,imppercent)
+
+# counts |> 
+#   filter(lemma_freq > 250) |> 
+#   distinct(lemma) |> 
+#   write_tsv('dat/lemma250.tsv')
+keep = read_tsv('dat/lemma250translated.tsv')
+
+counts |> 
+  inner_join(keep) |>  
+  left_join(implemma) |> 
+  mutate(
+    freq = sum(n),
+    percent = n/freq,
+    .by = c(lemma,source,translation)
+  ) |> 
+  mutate(
+    lemmatranslation = glue::glue('{lemma} ({translation})') |> 
+      fct_reorder(imppercent)
+         ) |> 
+  ggplot(aes(lemmatranslation,percent,fill = verb_past_class_complex)) +
+  geom_col() +
+  facet_wrap( ~ source) +
+  theme_few() +
+  scale_fill_colorblind() +
+  coord_flip() +
+  theme(
+    axis.title = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text.x = element_blank()
+  ) +
+  labs(fill = 'category')
+
+ggsave('viz/verbsfreq250gospel.png', dpi = 'print', width = 5, height = 8)
+
+d |> 
+  select(-translation) |> 
+  inner_join(keep) |> 
+  count(lemma,translation,lemma_freq,verb_past_class_complex,year) |> 
   mutate(
     sum = sum(n),
-    p = n/sum,
-    .by = c(translation,year,book,chapter)
+    percent = n/sum,
+    .by = c(lemma,translation,lemma_freq,year)
+  ) |> 
+  mutate(
+    lemmatranslation = glue::glue('{lemma} ({translation})') |> 
+      fct_reorder(-lemma_freq)
+  ) |> 
+  ggplot(aes(year,percent,colour = verb_past_class_complex)) +
+  geom_line() +
+  facet_wrap( ~ lemmatranslation) +
+  theme_few() +
+  scale_colour_colorblind() +
+  labs(colour = 'category') +
+  theme(
+    axis.title = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text.y = element_blank()
   )
 
-# -- fit -- #
-
-fit1 = glmer(n ~ verb_past_class_complex + translation + 
-               (1 | book) + 
-               (1 | book:chapter),
-             family = poisson, data = counts1)
-fit2 = glmer(n ~ verb_past_class_complex * translation + 
-               (1 | book) + 
-               (1 | book:chapter),
-             family = poisson, data = counts1)
-
-plot(compare_performance(fit1,fit2,metrics = 'common'))
-plots = plot(check_model(fit2))
-plots[[1]]
-plots[[2]]
-plots[[3]]
-plots[[4]]
-plots[[5]]
-plot_model(fit1, 'pred')
-
-# -- okay right -- #
-
-count(d,translation,year,book,chapter,verse)
+ggsave('viz/verbsfreq250gospelchange.png', dpi = 'print', width = 13, height = 6)
